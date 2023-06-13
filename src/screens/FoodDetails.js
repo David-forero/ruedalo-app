6d85b87ccf6c036f32ebf2eed9d64aff44eb2ae8
David Forero <david.forero1813@gmail.com>
2023-06-12 21:29:08 -0400

    trabajando loadings para detalles del producto

--- src/screens/FoodDetails.js
+++ src/screens/FoodDetails.js
@@ -1,199 +1,302 @@
 import {
-    View,
-    Text,
-    SafeAreaView,
-    ScrollView,
-    Image,
-    TouchableOpacity,
-    FlatList,
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  FlatList,
 } from "react-native";
 import React, { useState } from "react";
 import { useRoute, useNavigation } from "@react-navigation/native";
 
-import {
-    Header,
-    Button,
-} from "../common/components";
+import { Header, Button } from "../common/components";
 import { COLORS, FONTS, SAFEAREAVIEW, dummyData } from "../common/constants";
 import { Picker } from "@react-native-picker/picker";
 import { Rating } from "react-native-ratings";
 import { useEffect } from "react";
 import { useStoreContext } from "../context/StoreContext";
 import { useAuthContext } from "../context/AuthContext";
+import { Fade, Placeholder, PlaceholderLine } from "rn-placeholder";
 
 export default function FoodDetails() {
-    const navigation = useNavigation();
-    const {product, getProductFn} = useStoreContext();
-    const {user} = useAuthContext();
-    const [loading, setLoading] = useState(true);
-    const route = useRoute();
-    const [selectAcount, setSelectAcount] = useState(null);
-    const { id } = route.params;
+  const navigation = useNavigation();
+  const { product, getProductFn } = useStoreContext();
+  const { user } = useAuthContext();
+  const [loading, setLoading] = useState(true);
+  const route = useRoute();
+  const [selectAcount, setSelectAcount] = useState(null);
+  const { id } = route.params;
 
-    const [amount, setAmount] = useState(null);
-    
-    useEffect(() => {
-        setLoading(false);
-        getProductFn(id, user?.token, setLoading)
-        
-    }, [])
+  const [amount, setAmount] = useState(null);
 
-    useEffect(() => {
-        setAmount(product?.price)
-    }, [product?.price])
-    
+  useEffect(() => {
+    setLoading(true);
+    getProductFn(id, user?.token, setLoading);
+  }, []);
 
-    function renderDetails() {
-        return (
-            <View
-                style={{
-                    marginHorizontal: 30,
-                    // marginBottom: 20,
-                }}
-            >
-                <View>
-                    <Image
-                        source={{uri: 'https://repuestosya.cobrex.com.ve/api/product/' + product?.image[0]}}
-                        style={{
-                            height: 206,
-                            width: "100%",
-                            borderRadius: 14,
-                            marginBottom: 21,
-                        }}
-                        className="shadow-lg"
-                        resizeMode="stretch"
-                    />
-                    <Text
-                        style={{
-                            marginBottom: 8,
-                            ...FONTS.Roboto_500Medium,
-                            fontSize: 18,
-                            textTransform: "capitalize",
-                            color: COLORS.black,
-                        }}
-                    >
-                        {product?.title || 'Cargando...'}
-                    </Text>
-                    <Text
-                        style={{
-                            marginBottom: 12,
-                            ...FONTS.Roboto_400Regular,
-                            fontSize: 14,
-                            color: COLORS.gray2,
-                            lineHeight: 14 * 1.4,
-                            marginBottom: 10,
-                        }}
-                    >
-                        {/* {description} */}
-                       {product?.description}
-                    </Text>
-                    <View
-                        style={{
-                            flexDirection: "row",
-                            alignItems: "center",
-                            justifyContent: "space-between",
-                        }}
-                    >
-                        <Text
-                            style={{
-                                ...FONTS.Roboto_700Bold,
-                                fontSize: 20,
-                                color: COLORS.carrot,
-                            }}
-                        >
-                            ${product?.price}
-                        </Text>
-                    </View>
+  useEffect(() => {
+    setAmount(product?.price);
+  }, [product?.price]);
 
-                    <Text className='font-bold text-md mb-1 text-left text-gray-700 mt-5'>Vendido por</Text>
-
-                    <TouchableOpacity onPress={() => navigation.navigate("RestaurantMenu", { restaurant: dummyData[0] })} className=" flex-row space-x-3 mt-2">
-                        <Image
-                            source={{ uri: 'https://repuestosya.cobrex.com.ve/api/avatar/' + product?.commerce.avatar[0]}}
-                            className="h-10 w-10 rounded-full"
-                            resizeMode="stretch"
-                        />
-
-                        <View>
-                            <Text>{product?.commerce.registered_name}</Text>
-                            <View
-                                style={{
-                                    flexDirection: "row",
-                                    alignItems: "center",
-                                }}
-                            >
-                                <Rating
-                                    type="star"
-                                    count={5}
-                                    defaultRating={14}
-                                    imageSize={12}
-                                    showRating={false}
-                                    isDisabled={false}
-                                    readonly={true}
-                                    startingValue={product?.commerce.rating}
-                                />
-                                <Text
-                                    style={{
-                                        ...FONTS.Roboto_400Regular,
-                                        fontSize: 12,
-                                        color: COLORS.gray2,
-                                        marginLeft: 10,
-                                        lineHeight: 12 * 1.2,
-                                    }}
-                                >
-                                    ({product?.commerce.rating})
-                                </Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-
-                <View className="mt-10">
-                    <Text className='font-bold text-md mb-3 text-left text-gray-700'>Cantidad</Text>
-
-                    <Picker
-                        style={{
-                            width: '100%',
-                            height: 10,
-                            backgroundColor: COLORS.lightGray,
-                            borderRadius: 10,
-                            alignItems: "center",
-                            flexDirection: "row",
-                            marginBottom: 30
-                        }}
-                        mode="dropdown"
-                        selectedValue={selectAcount}
-                        onValueChange={(itemValue, itemIndex) => {
-                            setAmount(Number(itemValue) * Number(product?.price));
-                            setSelectAcount(itemValue)
-                        }}
-                    >
-
-                        <Picker.Item style={{ color: COLORS.gray2, marginLeft: 10 }} label="1" value="1" />
-                        <Picker.Item style={{ color: COLORS.gray2, marginLeft: 10 }} label="2" value="2" />
-                        <Picker.Item style={{ color: COLORS.gray2, marginLeft: 10 }} label="3" value="3" />
-                    </Picker>
-                </View>
-
-                <Button
-                    title={loading ? 'Cargando...' : `Comprar por $${amount}` }
-                    containerStyle={{ marginBottom: 20, marginTop: 60 }}
-                    onPress={() => {
-                        navigation.navigate('PaymentMethodOne', {amount, product, unit: selectAcount})
-                    }}
-                />
-            </View>
-        );
-    }
-
+  function renderDetails() {
     return (
-        <SafeAreaView style={{ ...SAFEAREAVIEW.AndroidSafeArea }}>
-            <Header title="Detalles del producto" onPress={() => navigation.goBack()} />
-            <ScrollView
-                showsVerticalScrollIndicator={false}
+      <View
+        style={{
+          marginHorizontal: 30,
+          // marginBottom: 20,
+        }}
+      >
+        <View>
+          {loading ? (
+            <View
+              style={{
+                height: 206,
+                width: "100%",
+                borderRadius: 14,
+                marginBottom: 21,
+              }}
+              className="shadow-lg bg-gray-300"
+              resizeMode="stretch"
+            ></View>
+          ) : (
+            <Image
+              source={{
+                uri:
+                  "https://repuestosya.cobrex.com.ve/api/product/" +
+                  product?.image[0],
+              }}
+              style={{
+                height: 206,
+                width: "100%",
+                borderRadius: 14,
+                marginBottom: 21,
+              }}
+              className="shadow-lg"
+              resizeMode="stretch"
+            />
+          )}
+          {loading ? (
+            <Placeholder Animation={Fade}>
+              <PlaceholderLine width={80} />
+            </Placeholder>
+          ) : (
+            <Text
+              style={{
+                marginBottom: 8,
+                ...FONTS.Roboto_500Medium,
+                fontSize: 18,
+                textTransform: "capitalize",
+                color: COLORS.black,
+              }}
             >
-                {renderDetails()}
-            </ScrollView>
-        </SafeAreaView>
+              {product?.title}
+            </Text>
+          )}
+
+          {loading ? (
+            <Placeholder Animation={Fade} style={{ marginTop: 30 }}>
+              <PlaceholderLine width={50} />
+              <PlaceholderLine width={76} />
+              <PlaceholderLine width={43} />
+              <PlaceholderLine width={78} />
+              <PlaceholderLine width={50} />
+            </Placeholder>
+          ) : (
+            <Text
+              style={{
+                marginBottom: 12,
+                ...FONTS.Roboto_400Regular,
+                fontSize: 14,
+                color: COLORS.gray2,
+                lineHeight: 14 * 1.4,
+                marginBottom: 10,
+              }}
+            >
+              {/* {description} */}
+              {product?.description}
+            </Text>
+          )}
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text
+              style={{
+                ...FONTS.Roboto_700Bold,
+                fontSize: 20,
+                color: COLORS.carrot,
+              }}
+            >
+              ${product?.price}
+            </Text>
+          </View>
+
+          {loading ? (
+            <Placeholder Animation={Fade} style={{marginTop: 10}}>
+              <PlaceholderLine width={30} />
+            </Placeholder>
+          ) : (
+            <Text className="font-bold text-md mb-1 text-left text-gray-700 mt-5">
+              Vendido por
+            </Text>
+          )}
+
+          {loading ? (
+            <View className=" flex-row space-x-3 mt-2">
+              <View
+                className="h-10 w-10 rounded-full bg-gray-300"
+                resizeMode="stretch"
+              ></View>
+
+              <View>
+                <Placeholder Animation={Fade}>
+                  <PlaceholderLine width={50} />
+                </Placeholder>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <Placeholder Animation={Fade}>
+                    <PlaceholderLine width={40} />
+                  </Placeholder>
+                  <Placeholder Animation={Fade}>
+                    <PlaceholderLine width={10} />
+                  </Placeholder>
+                </View>
+              </View>
+            </View>
+          ) : (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("RestaurantMenu", {
+                  restaurant: dummyData[0],
+                })
+              }
+              className=" flex-row space-x-3 mt-2"
+            >
+              <Image
+                source={{
+                  uri:
+                    "https://repuestosya.cobrex.com.ve/api/avatar/" +
+                    product?.commerce.avatar[0],
+                }}
+                className="h-10 w-10 rounded-full"
+                resizeMode="stretch"
+              />
+
+              <View>
+                <Text>{product?.commerce.registered_name}</Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <Rating
+                    type="star"
+                    count={5}
+                    defaultRating={14}
+                    imageSize={12}
+                    showRating={false}
+                    isDisabled={false}
+                    readonly={true}
+                    startingValue={product?.commerce.rating}
+                  />
+                  <Text
+                    style={{
+                      ...FONTS.Roboto_400Regular,
+                      fontSize: 12,
+                      color: COLORS.gray2,
+                      marginLeft: 10,
+                      lineHeight: 12 * 1.2,
+                    }}
+                  >
+                    ({product?.commerce.rating})
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+        </View>
+
+      {loading ? (
+        <View className="mt-10">
+ <Placeholder Animation={Fade}>
+                    <PlaceholderLine width={40} />
+                  </Placeholder>
+        </View>
+      ) : (
+        <View className="mt-10">
+        <Text className="font-bold text-md mb-3 text-left text-gray-700">
+          Cantidad
+        </Text>
+
+        <Picker
+          style={{
+            width: "100%",
+            height: 10,
+            backgroundColor: COLORS.lightGray,
+            borderRadius: 10,
+            alignItems: "center",
+            flexDirection: "row",
+            marginBottom: 30,
+          }}
+          mode="dropdown"
+          selectedValue={selectAcount}
+          onValueChange={(itemValue, itemIndex) => {
+            setAmount(Number(itemValue) * Number(product?.price));
+            setSelectAcount(itemValue);
+          }}
+        >
+          <Picker.Item
+            style={{ color: COLORS.gray2, marginLeft: 10 }}
+            label="1"
+            value="1"
+          />
+          <Picker.Item
+            style={{ color: COLORS.gray2, marginLeft: 10 }}
+            label="2"
+            value="2"
+          />
+          <Picker.Item
+            style={{ color: COLORS.gray2, marginLeft: 10 }}
+            label="3"
+            value="3"
+          />
+        </Picker>
+      </View>
+      )}
+
+        <Button
+          title={loading ? "Cargando..." : `Comprar por $${amount}`}
+          containerStyle={{ marginBottom: 20, marginTop: 60 }}
+          onPress={() => {
+            navigation.navigate("PaymentMethodOne", {
+              amount,
+              product,
+              unit: selectAcount,
+            });
+          }}
+        />
+      </View>
     );
+  }
+
+  return (
+    <SafeAreaView style={{ ...SAFEAREAVIEW.AndroidSafeArea }}>
+      <Header
+        title="Detalles del producto"
+        onPress={() => navigation.goBack()}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {renderDetails()}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
